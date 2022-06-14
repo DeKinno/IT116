f3155f90cb4d449fb2672e659bb7c1529dbe156a
DeKinno <egnavas173@gmial.com>
2022-06-13 19:46:43 -0700

    conditions1

---
+++ conditions1.js
@@ -0,0 +1,32 @@
+console.log("Hello world");
+
+let randomNumber = Math.random(); 
+
+// if (randomNumber < 0.50) {
+
+//     console.log("Condition was Met");
+//     console.log(randomNumber);
+
+// }
+
+
+// 
+if (randomNumber >= 0.5) {
+
+    console.log('Condition was Met');
+    console.log(randomNumber);
+
+}
+
+const dayOfWeek = 'saturday';
+if (dayOfWeek === 'Monday') {
+
+console.log("Yay its Monday");
+
+
+} else if (dayOfWeek === 'Friday'){
+
+    console.log("yay its friday");
+}else if (dayOfWeek === 'saturday') {
+    console.log("You have the correct day from the variable");
+}
